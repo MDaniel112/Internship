604c639f94cccd14613227c0619288122fd7757c
Mocanu Daniel - Teodor <79073896+MDaniel112@users.noreply.github.com>
2021-09-27 17:12:35 +0300

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,75 @@
+console.log("*****Arrow functions*****");
+let rotunjeste = nr => Math.round(nr);  
+
+let toArray = sir => sir.split(" ");
+
+const pi = 3.14;
+
+let arieCerc = raza => pi * Math.pow(raza, 2);
+
+console.log(rotunjeste(5.55));
+let arr = toArray("Acesta este primul task")
+console.log(arr);
+console.log(arieCerc(3));
+
+arr.forEach(cuv => console.log(cuv, "= ", cuv.length, " "));
+
+//spread operator
+console.log("*****Spread Operator*****")
+let arr1 = ["este", "primul", "task"];
+let arr2 = ["Acesta", ...arr1];
+console.log(arr2);
+
+//obiecte
+function Judet(nume, municipiu, orase){
+    this.nume  = nume;
+    this.municipiu = municipiu;
+    this.orase = orase;
+    this.despre= function() {
+        console.log(`Judetul ${this.nume}, municipiul ${this.municipiu}`);
+    }
+}
+
+const Braila = new Judet("Braila", "Braila", ["Braila", "Ianca", "Insuratei"]);
+Braila.despre();
+
+//iterare obiect
+console.log("*****Iterare Obiect*****")
+for(const atr in Braila){
+    console.log(`${atr}= ${Braila[atr]}`);
+}
+
+//deep clone
+console.log("*****Deep Copy*****")
+const deepCopy = JSON.parse(JSON.stringify(Braila))
+console.log(deepCopy);
+//metode ES6
+console.log("*****Object Property Assignment*****")
+let pop = {populatie: 200000};
+Object.assign(Braila, pop);
+console.log("Populatie= ",Braila.populatie);
+
+console.log("*****Array Element Finding*****")
+console.log(Braila.orase.find(x => x == "Ianca"));
+
+console.log("*****String Repeating*****");
+console.log("Braila".repeat(4));
+
+console.log("*****String Searching*****");
+console.log("Orasul Braila".includes("Braila"));
+
+console.log("*****Number Type Checking*****");
+console.log(Number.isNaN(42));
+
+console.log("*****Number Safety Checking*****");
+console.log(Number.isSafeInteger(42))
+
+console.log("*****Number Comparison*****");
+console.log(2 + 3 === 5);
+
+console.log("*****Number Truncation*****");
+console.log(Math.trunc(420.07));
+
+console.log("*****Number Sign Determination*****");
+console.log(Math.sign(5));
+console.log(Math.sign(-5));
